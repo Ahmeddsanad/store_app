3661f7af737687065a24de08a91b15558c75c606
Ahmed Sanad <ahmed.hac26@gmail.com>
2023-08-15 04:43:43 +0300

    put_method

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -40,4 +40,39 @@ class api {
           'there is a problem with status code ${response.statusCode} with body ${jsonDecode(response.body)}');
     }
   }
+
+  Future<dynamic> put(
+      {required String url, @required dynamic body, String? token}) async {
+    Map<String, String> headers = {};
+
+    headers.addAll({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+
+    if (token != null) {
+      headers.addAll({
+        'Authorization': 'Bearer $token',
+      });
+    }
+
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+
+      return data;
+    } else {
+      throw Exception(
+          'there is a problem with status code ${response.statusCode} with body ${jsonDecode(response.body)}');
+    }
+  }
 }
+
+
+
+// put always be 'application/x-www-form-urlencoded'
+// post always be 'application/form-data'
