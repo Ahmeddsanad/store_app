72db31b8ac85a7ad964de3313cea2a305384b61f
Ahmed Sanad <ahmed.hac26@gmail.com>
2023-08-16 10:03:38 +0300

    displaying_api_data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:store_app/models/product_model.dart';
+import 'package:store_app/services/get_all_product_services.dart';
 import 'package:store_app/widgets/custom_card.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -33,22 +35,41 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.only(
-            top: 65,
-            left: 16,
-            right: 16,
-          ),
-          child: GridView.builder(
-            clipBehavior: Clip.none,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 1.5,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 100,
+            padding: const EdgeInsets.only(
+              top: 65,
+              left: 16,
+              right: 16,
             ),
-            itemBuilder: (context, index) => const CustomCard(),
-          ),
-        ),
+            child: FutureBuilder<List<ProductModel>>(
+              future: AllProductsService().getAllProduct(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<ProductModel> products = snapshot.data!;
+
+                  return GridView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    clipBehavior: Clip.none,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1.5,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 100,
+                    ),
+                    itemBuilder: (context, index) => CustomCard(
+                      products: products[index],
+                    ),
+                    itemCount: products.length,
+                  );
+                } else {
+                  print(AllProductsService().getAllProduct());
+                  return Center(
+                      heightFactor: 30,
+                      child: CircularProgressIndicator(
+                        color: Colors.black,
+                      ));
+                }
+              },
+            )),
       ),
     );
   }
